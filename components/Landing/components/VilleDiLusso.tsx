0cb1d3f16e011f22da865ca78846aa98e03976bc
auritiwdesign <juancamilo.auriti@gmail.com>
2024-03-05 06:26:49 +0100

    update component carousel mobile view

--- components/Landing/components/VilleDiLusso.tsx
+++ components/Landing/components/VilleDiLusso.tsx
@@ -17,9 +17,9 @@ export default function VilleDiLusso() {
         <section className='py-4 px-2 lg:px-48 w-full'>
             <div className='py-6'>
                 <div className="p-8 lg:p-40 bg-accent">
-                    <div className='flex gap-4 lg:items-center'>
+                    <div className='flex gap-4 lg:items-center items-center'>
                         <div
-                            className='before:content-[] h-16 w-0 lg:h-0 lg:w-28 border-2 lg:border border-primary'></div>
+                            className='before:content-[] h-10 w-0 lg:h-0 lg:w-28 border-2 lg:border border-primary'></div>
                         <h2 className='font-serif text-2xl lg:text-5xl'>Ville di Lusso</h2>
                     </div>
 
@@ -27,7 +27,7 @@ export default function VilleDiLusso() {
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus alias atque blanditiis commodi cumque delectus doloribus dolorum est eum expedita explicabo harum iste itaque iusto nihil nisi nobis numquam, officia perferendis possimus quae quia ratione recusandae rem sequi similique.</p>
                     </div>
 
-                    <div className="p-4">
+                    <div className="py-4">
                         <Carousel
                             plugins={[
                                 Autoplay({
@@ -41,28 +41,28 @@ export default function VilleDiLusso() {
                             className="w-full"
                         >
                             <CarouselContent>
-                                <CarouselItem  className="md:basis-1/2 lg:basis-1/3">
+                                <CarouselItem  className="md:basis-1/2 lg:basis-1/3 basis-1/2">
                                     <AspectRatio ratio={16 / 9}>
                                         <Image src={test1} alt="Test 1" width={1080} height={450}/>
                                     </AspectRatio>
                                 </CarouselItem>
-                                <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                                <CarouselItem className="md:basis-1/2 lg:basis-1/3 basis-1/2">
                                     <AspectRatio ratio={16 / 9}>
                                     <Image src={test2} alt="Test 2" width={1080} height={450} />
                                     </AspectRatio>
                                 </CarouselItem>
-                                <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                                <CarouselItem className="md:basis-1/2 lg:basis-1/3 basis-1/2">
                                     <AspectRatio ratio={16 / 9}>
                                     <Image src={test3} alt="Test 3" width={1080} height={450} />
                                     </AspectRatio>
                                 </CarouselItem>
 
-                                <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                                <CarouselItem className="md:basis-1/2 lg:basis-1/3 basis-1/2">
                                     <AspectRatio ratio={16 / 9}>
                                     <Image src={test4} alt="Test 4" width={1080} height={450}  />
                                     </AspectRatio>
                                 </CarouselItem>
-                                <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                                <CarouselItem className="md:basis-1/2 lg:basis-1/3 basis-1/2">
                                     <AspectRatio ratio={16 / 9}>
                                     <Image src={test5} alt="Test 5" width={1080} height={450} />
                                     </AspectRatio>
