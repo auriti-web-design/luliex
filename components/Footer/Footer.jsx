5176165b320b2d4c56ec28aa5908503feb4a66f9
auritiwdesign <juancamilo.auriti@gmail.com>
2024-03-06 11:59:22 +0100

    Create Footer.jsx

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+        <div className="flex flex-col items-center justify-center w-full h-full py-4 border-t">
+            <p className="text-center text-sm text-gray-400">
+                © 2024 Luliex. All rights reserved.
+            </p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
